c08eb08a0e887b1b316cb08f741e196fa84bc439
ggg1psyyy <5058035+ggg1psyyy@users.noreply.github.com>
2024-12-10 12:12:38 +0100

    first commit of managers

---
+++ apps/PVControl/Managers/HeatpumpManager.cs
@@ -0,0 +1,127 @@
+﻿using NetDaemon.HassModel.Entities;
+using System.Threading.Tasks;
+using static NetDeamon.apps.PVControl.PVControlCommon;
+
+namespace NetDeamon.apps.PVControl
+{
+  public partial class PVConfig
+  {
+    public Entity WarmwaterTemperatureEntity { get; set; } = null!;
+    public Entity WarmwaterSetPointNormalEntity { get; set; } = null!;
+    public Entity WarmwaterSetPointOnceEntity { get; set; } = null!;
+    public Entity WarmwaterEnergyUsageEntity { get; set; } = null!;
+    public Entity WarmwaterStartOnceEntity { get; set; } = null!;
+  }
+}
+
+namespace NetDeamon.apps.PVControl.Managers
+{
+
+  internal class HeatpumpManager : ILoadManager
+  {
+    private PowerRequest _PowerRequest;
+    public PowerRequest Initialize()
+    {
+      _PowerRequest = new PowerRequest
+      {
+        LoadManager = this,
+        CanBeInterrupted = true,
+        RequestStatus = PowerReqestStatus.Idle,
+        RequestedUpdateRate = 15,
+        LastUpdate = DateTime.Now,
+        RequestDescription = "Heatpump - Warmwater",
+      };
+      Update();
+      return _PowerRequest;
+    }
+
+    public void Pause()
+    {
+      if (PVCC_Config.WarmwaterStartOnceEntity.TurnOff())
+        _PowerRequest.RequestStatus = PowerReqestStatus.Paused;
+      else
+        _PowerRequest.RequestStatus = PowerReqestStatus.Error;
+    }
+    int _startEnergyUsage = 0;
+    float _startWWTemp = 0;
+    public void Start()
+    {
+      Update();
+      if (_PowerRequest.RequestStatus == PowerReqestStatus.Running)
+      {
+
+      }
+      else if (PVCC_Config.WarmwaterTemperatureEntity.TryGetStateValue(out _startWWTemp) && PVCC_Config.WarmwaterSetPointOnceEntity.TryGetStateValue(out float setValue))
+      {
+        _startEnergyUsage = 0;
+        if (PVCC_Config.WarmwaterEnergyUsageEntity.TryGetStateValue(out int val))
+          _startEnergyUsage = val;
+        if (_startWWTemp < (setValue - 8))
+        {
+          if (PVCC_Config.WarmwaterStartOnceEntity.TurnOn())
+          {
+            _PowerRequest.RequestStatus = PowerReqestStatus.Running;
+            PVCC_Logger.LogInformation("Started WarmWater");
+          }
+          else
+          {
+            _PowerRequest.RequestStatus = PowerReqestStatus.Error;
+            PVCC_Logger.LogInformation("Error starting WarmWater");
+          }
+          _PowerRequest.EnergyUsed = _startEnergyUsage;
+        }
+        else
+          _PowerRequest.RequestStatus = PowerReqestStatus.Idle;
+      }
+      else
+        _PowerRequest.RequestStatus = PowerReqestStatus.Error;
+
+      _PowerRequest.LastUpdate = DateTime.Now;
+    }
+
+    public void Stop()
+    {
+      if (PVCC_Config.WarmwaterStartOnceEntity.TurnOff())
+      {
+        _PowerRequest.RequestStatus = PowerReqestStatus.Idle;
+        PVCC_Logger.LogInformation("Stoped WarmWater");
+      }
+      else
+        _PowerRequest.RequestStatus = PowerReqestStatus.Error;
+
+      if (PVCC_Config.WarmwaterEnergyUsageEntity.TryGetStateValue(out int val))
+        _PowerRequest.EnergyUsed = val - _startEnergyUsage;
+    }
+
+    public void Update()
+    {
+      if (PVCC_Config.WarmwaterTemperatureEntity.TryGetStateValue(out float curTemp) 
+        && PVCC_Config.WarmwaterSetPointOnceEntity.TryGetStateValue(out float targetTemp)
+        && PVCC_Config.WarmwaterSetPointNormalEntity.TryGetStateValue(out float minTemp)
+        && PVCC_Config.WarmwaterStartOnceEntity.TryGetStateValue(out bool isActive))
+      {
+        float diffToMin = curTemp - minTemp;
+        if (diffToMin - minTemp < 5)
+        {
+        }
+        //if (PVCC_Config.WarmwaterEnergyUsageEntity.TryGetStateValue(out int val))
+        //  _PowerRequest.EnergyUsed = val - _startEnergyUsage;
+        if (isActive)
+        {
+          if (curTemp >= targetTemp)
+          {
+            Stop();
+          }
+          else
+            _PowerRequest.RequestStatus = PowerReqestStatus.Running;
+        }
+        else
+          _PowerRequest.RequestStatus = PowerReqestStatus.Idle;
+      }
+      else
+        _PowerRequest.RequestStatus = PowerReqestStatus.Error;
+
+      _PowerRequest.LastUpdate = DateTime.Now;
+    }
+  }
+}
---
+++ apps/PVControl/Managers/Manager.cs
@@ -0,0 +1,72 @@
+﻿using NetDaemon.Extensions.Scheduler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static NetDeamon.apps.PVControl.PVControlCommon;
+
+namespace NetDeamon.apps.PVControl.Managers
+{
+  public enum PowerReqestStatus
+  {
+    Idle = 0,
+    Requested = 1,
+    Running = 2,
+    Paused = 4,
+    Stopped = 8,
+    Error = 16,
+  }
+  public struct PowerRequest
+  {
+    public ILoadManager LoadManager;
+    public PowerReqestStatus RequestStatus;
+    public int EstimatedEnergyNeeded; // in Wh
+    public int EstimatedTimeNeeded; // in minutes
+    public DateTime PreferedStartTime;
+    public DateTime LatestStartTime;
+    public int EnergyUsed;
+    public int RunTime;
+    public bool CanBeInterrupted;
+    public int RequestedUpdateRate; // in minutes
+    public DateTime LastUpdate;
+    public string RequestDescription;
+  }
+  public interface ILoadManager
+  {
+    public PowerRequest Initialize();
+    public void Start();
+    public void Stop();
+    public void Pause();
+    public void Update();
+  }
+  public class Manager
+  {
+    private HouseEnergy _house;
+    private List<PowerRequest> _powerRequests;
+
+    public Manager(HouseEnergy house)
+    {
+      _house = house;
+      PVCC_Scheduler.ScheduleCron("*/30 * * * * *", async () => await ScheduledOperations(), true);
+      _powerRequests = [];
+      _powerRequests.Add(new HeatpumpManager().Initialize());
+
+      _ = ScheduledOperations();
+    }
+    private async Task ScheduledOperations()
+    {
+      DateTime now = DateTime.Now;
+      foreach (var powerRequest in _powerRequests)
+      {
+        if (DateTime.Now.AddMinutes(-powerRequest.RequestedUpdateRate) > powerRequest.LastUpdate)
+          powerRequest.LoadManager.Update();
+        if (_house.IsNowCheapestWindowToday && powerRequest.RequestStatus != PowerReqestStatus.Running)
+        {
+          powerRequest.LoadManager.Start();
+        }
+      }
+    }
+  }
+}
